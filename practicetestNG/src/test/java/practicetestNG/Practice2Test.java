7be1d6143116d23647fb1e4ab31a5068910962bf
5Nanda <109574707+5Nanda@users.noreply.github.com>
2022-07-19 13:05:22 +0530

    Update Practice2Test.java

--- practicetestNG/src/test/java/practicetestNG/Practice2Test.java
+++ practicetestNG/src/test/java/practicetestNG/Practice2Test.java
@@ -5,7 +5,8 @@ import org.testng.annotations.Test;
 public class Practice2Test {
 	@Test
 public static void test() {
- System.out.println("Test_02");	
+ System.out.println("Test_02_11");	
+System.out.println("Test_02_12");	
  String BROWSER = System.getProperty("browser");
  System.out.println(BROWSER);
 }
